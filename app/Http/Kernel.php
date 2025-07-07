a9b745c875b3541cc26b882902964e245397e88f
zhfrann <muhzhafranilham13@gmail.com>
2025-07-07 09:45:46 +0700

    feat(middleware): add middleware to set default Accept header using application/json

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,8 +40,9 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\DefaultJsonAcceptHeader::class
         ],
     ];
 
@@ -63,5 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        // 'accept-json' => \App\Http\Middleware\DefaultJsonAcceptHeader::class
     ];
 }
