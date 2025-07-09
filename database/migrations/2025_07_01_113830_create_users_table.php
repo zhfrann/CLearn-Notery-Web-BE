bae34babeeb1651743e209ce284b8985441b3fb9
zhfrann <muhzhafranilham13@gmail.com>
2025-07-10 01:40:26 +0700

    refactor: remove rating from users table

--- database/migrations/2025_07_01_113830_create_users_table.php
+++ database/migrations/2025_07_01_113830_create_users_table.php
@@ -27,7 +27,7 @@ return new class extends Migration
             // $table->string('matkul_favorit')->nullable();
             $table->foreignId('matkul_favorit')->nullable()->constrained('courses', 'course_id')->cascadeOnUpdate()->cascadeOnDelete();
             $table->string('foto_profil')->nullable();
-            $table->decimal('rating', 2, 1)->default(0);
+            // $table->decimal('rating', 2, 1)->default(0);
             // $table->timestamp('email_verified_at')->nullable();
             // $table->rememberToken();
             $table->timestamps();
