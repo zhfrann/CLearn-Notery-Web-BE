b1f125239fab0f8a1539bb4f0a7ba9addfe09213
excellencenachua <excellencenachua@gmail.com>
2025-07-13 19:44:32 +0700

    chuaprofile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Resources\UserResource;
 
 class ProfileController extends Controller
 {
-    public function getProfileDetail(Request $request) {}
+    public function getProfileDetail(Request $request) {
+
+        $user = User::query()->where('user_id', $request->user()->user_id)->first();
+
+        return response()->json([
+            'success' => true,
+            'message' => '',
+            'data' => new UserResource($user->load(['semester', 'major', 'faculty']))
+        ]);
+    }
 
     public function updateProfile(Request $request) {}
 
@@ -15,4 +26,7 @@ class ProfileController extends Controller
     // public function updatePhoto(Request $request) {}
 
     // public function changePassword(Request $request) {}
+
+
 }
+
